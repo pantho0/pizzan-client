a931df66f654096a9e5c2977caac6d98750142af
pantho0 <panthomashreky6@gmail.com>
2023-11-06 22:26:26 +0600

    spinner added for all foods page

--- src/Pages/Food/AllFood.jsx
+++ src/Pages/Food/AllFood.jsx
@@ -26,13 +26,18 @@ const AllFood = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
                         </button>
                     </div>
-                    <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10'>
+                    {
+                        !isLoading ?
+                        <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-4 mt-10'>
                         {/* Foods Card */}
-                        {   !isLoading ? foods.map(food => <FoodsCard key={food._id} food={food}></FoodsCard>)
-                            : 
-                            <div>Loading</div>
-                        }
+                        { foods.map(food => <FoodsCard key={food._id} food={food}></FoodsCard>)}
                     </div>
+                        : 
+                    <div className='flex justify-center items-center min-h-screen'>
+                        <progress className="progress w-56"></progress>
+                    </div>
+                    }
+                    
                 </div>
             </div>
         </div>
